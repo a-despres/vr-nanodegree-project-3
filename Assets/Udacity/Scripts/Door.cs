d32553594cc2d217321ac734a3a7bbc0716fc432
Andy Despres <adespres@gmail.com>
2016-11-25 21:49:57 -0800

    Key and Door Added
    A key was added to the maze that unlocks the door at the temple. The
    door will not open without the key.

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Door : MonoBehaviour 
 {
-    // Create a boolean value called "locked" that can be checked in Update() 
+	public AudioClip lockedAudioClip;
+	public AudioClip unlockedAudioClip;
 
-    void Update() {
+    // Create a boolean value called "locked" that can be checked in Update() 
+	bool locked = true;
+
+	// "doorFullyRaised" is set to true by default so that the door does not open automatically when the key is collected
+	bool doorFullyRaised = true;
+
+    void Update () {
         // If the door is unlocked and it is not fully raised
-            // Animate the door raising up
+		if (!locked && !doorFullyRaised) {
+			// Animate the door raising up
+			gameObject.transform.Translate(0, 10f * Time.deltaTime, 0);
+		}
     }
 
-    public void Unlock()
+	public void OnDoorClicked () {
+		AudioSource doorAudio = gameObject.GetComponent<AudioSource>();
+
+		if (!locked) { 
+			doorFullyRaised = false;
+			doorAudio.clip = unlockedAudioClip;
+			doorAudio.Play ();
+		} else {
+			Debug.Log ("Door locked");
+			doorAudio.clip = lockedAudioClip;
+			doorAudio.Play ();
+		}
+	}
+
+    public void Unlock ()
     {
-        // You'll need to set "locked" to true here
+        // You'll need to set "locked" to false here
+		locked = false;
     }
 }
--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Key : MonoBehaviour 
 {
     //Create a reference to the KeyPoofPrefab and Door
+	public GameObject keyPoof;
+	public Door door;
 
 	void Update()
 	{
 		//Bonus: Key Animation
+		transform.Rotate (0, 50.0f * Time.deltaTime, 0);
 	}
 
 	public void OnKeyClicked()
 	{
         // Instatiate the KeyPoof Prefab where this key is located
-        // Make sure the poof animates vertically
+		// Make sure the poof animates vertically
+		Vector3 positionOfKey = new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.transform.position.z);
+		Instantiate(keyPoof, positionOfKey, Quaternion.Euler(-90, 0, 0));
+
         // Call the Unlock() method on the Door
-        // Destroy the key. Check the Unity documentation on how to use Destroy
+		door.Unlock ();
+
+		// Destroy the key. Check the Unity documentation on how to use Destroy
+		Destroy (gameObject);
     }
 
 }
