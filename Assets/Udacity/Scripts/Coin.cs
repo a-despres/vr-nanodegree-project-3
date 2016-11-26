85bda80ea4cf7384638646bbf3ea8579f5575efb
Andy Despres <adespres@gmail.com>
2016-11-25 18:54:05 -0800

    Coins Added
    Added 10 coins to the maze that can be collected by the player.

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour 
 {
-    //Create a reference to the CoinPoofPrefab
+	public GameObject coinPoof;
 
-    public void OnCoinClicked() {
-        // Instantiate the CoinPoof Prefab where this coin is located
-        // Make sure the poof animates vertically
+    public void OnCoinClicked () {
+		// Instantiate the CoinPoof Prefab where this coin is located
+		// Make sure the poof animates vertically
+		Vector3 positionOfCoin = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.transform.position.z);
+		Instantiate(coinPoof, positionOfCoin, Quaternion.Euler(-90, 0, 0));
+
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+		Destroy (gameObject);
     }
-
 }
