9da14d3f7b9d5cf04cb503ee93f4461e23f090a8
Andy Despres <adespres@gmail.com>
2016-11-27 12:12:39 -0800

    Adjusted Lighting and Materials and Added Sunset Script
    The sunset script slowly animates the sun in the sky. When the sun
    reaches the horizon, and flashlight attached the player is
    automatically switched on.

---
+++ Assets/Custom/Scripts/Sunset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sunset : MonoBehaviour {
+
+	public GameObject sun;
+	public GameObject flashlight;
+
+	private bool isFlashlightOn = false;
+
+	// Update is called once per frame
+	void Update () {
+		Quaternion startRotation = Quaternion.Euler (45f, 30f, 0f);
+		Quaternion endRotation = startRotation * Quaternion.Euler (0f, 180f, 0f);
+		sun.transform.rotation = Quaternion.Slerp (startRotation, endRotation, Time.time / 300f);
+
+		// When the sun reaches the horizon, turn the flashlight on
+		if (sun.transform.rotation.x < 0.2 && isFlashlightOn == false) {
+			flashlight.SetActive(true);
+		}
+	}
+}
