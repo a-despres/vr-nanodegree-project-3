54a81f5638b906825b914e6fd4944c170efb57e0
Andy Despres <adespres@gmail.com>
2016-11-27 19:45:51 -0800

    Fixed sunset script
    Fixed sunset script so that the sun and flashlights reset when the
    scene is reloaded when the player completes the maze.

--- Assets/Custom/Scripts/Sunset.cs
+++ Assets/Custom/Scripts/Sunset.cs
@@ -6,17 +6,33 @@ public class Sunset : MonoBehaviour {
 	public GameObject sun;
 	public GameObject flashlight;
 
+	Quaternion startRotation;
+
 	private bool isFlashlightOn = false;
+
+	void Awake () {
+		startRotation = Quaternion.Euler (45f, 30f, 0f);
+	}
+
+	void Start () {
+		flashlight.SetActive (false);
+		sun.transform.rotation = startRotation;
+	}
 
 	// Update is called once per frame
 	void Update () {
-		Quaternion startRotation = Quaternion.Euler (45f, 30f, 0f);
-		Quaternion endRotation = startRotation * Quaternion.Euler (0f, 180f, 0f);
-		sun.transform.rotation = Quaternion.Slerp (startRotation, endRotation, Time.time / 300f);
+//		sun.transform.rotation = Quaternion.Slerp (startRotation, endRotation, Time.time / 300f);
+		sun.transform.Rotate(0f, 1f * Time.deltaTime, 0f);
 
-		// When the sun reaches the horizon, turn the flashlight on
-		if (sun.transform.rotation.x < 0.2 && isFlashlightOn == false) {
-			flashlight.SetActive(true);
+		// When the sun moves below the horizon, turn the flashlight on
+		if (sun.transform.rotation.eulerAngles.x > 180 && !isFlashlightOn) {
+			Debug.Log ("Flashlight turned on.");
+			flashlight.SetActive (true);
+			isFlashlightOn = true;
+		} else if (sun.transform.rotation.eulerAngles.x > 0 && sun.transform.rotation.eulerAngles.x < 180 && isFlashlightOn) {
+			Debug.Log ("Flashlight turned off.");
+			flashlight.SetActive (false);
+			isFlashlightOn = false;
 		}
 	}
 }
